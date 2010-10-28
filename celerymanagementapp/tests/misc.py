14ece72c1bac8c2fb179948347bbbc39c6f32658
David White <dpwhite2@ncsu.edu>
2010-10-28 10:37:25 -0400

    Removed errant test case.

--- celerymanagementapp/tests/misc.py
+++ celerymanagementapp/tests/misc.py
@@ -28,22 +28,6 @@ class CalculateThroughput_TestCase(base.CeleryManagement_DBTestCaseBase):
             self.assertEquals(len(seq),9)  # 9 = 45/5
             self.assertEquals(sum(seq), 0.)
     
-    def test_single_task(self):
-        # Dispatch task, then make sure *something* shows up in the throughput 
-        # results.
-        with TaskContextManager():
-            print 'enter time:             {0}'.format(datetime.datetime.now())
-            tasks.simple_test.apply_async()
-            time.sleep(5.)
-            taskname = 'celerymanagementapp.testutil.tasks.simple_test'
-            now = datetime.datetime.now()
-            timerange = (now-datetime.timedelta(seconds=120), now)
-            interval = 5
-            seq = calculate_throughputs(taskname, timerange, interval)
-            print 'after throughput time:  {0}'.format(datetime.datetime.now())
-            
-            self.assertEquals(len(seq),24)  # 24 = 120/5
-            self.assertTrue(sum(seq) > 0., 'failed: {0} > 0.'.format(sum(seq)))
         
         
         
