511dbf0187bb11e59efd8f446ff1c415f848a052
Dennis Kliban <dkliban@ncsu.edu>
2010-11-01 18:40:46 -0400

    Added task list and visualization for runtime distribution.

--- celerymanagementapp/urls.py
+++ celerymanagementapp/urls.py
@@ -10,10 +10,13 @@ DATERANGE2 = '(?:_'+DATEMAX+')'
 USERNAME = r'(?P<username>[a-zA-Z0-9]+)'##JOBID = r'(?:_job_(?P<jobid>[0-9]+))'
 
 urlpatterns = patterns('celerymanagementapp',
+    (r'^view/tasks/$', 'views.view_defined_tasks'),
     (r'^view/throughputs/$', 'views.view_throughputs'),
     (r'^view/throughputs/(?P<taskname>[-\w\d_.]+)/$', 'views.view_throughputs'),
     (r'^get/throughputs/(?P<taskname>[-\w\d_.]+)/$', 'views.get_throughput_data'),
+    (r'^get/runtimes/(?P<taskname>[-\w\d_.]+)/(?P<bin_count>[-\w\d_.]+)/(?P<bin_size>[-\w\d_.]+)/$', 'views.get_runtime_data'),
     (r'^visualize/throughputs/(?P<taskname>[-\w\d_.]+)/$', 'views.visualize_throughput'),
+    (r'^visualize/runtimes/(?P<taskname>[-\w\d_.]+)/(?P<bin_count>[-\w\d_.]+)/(?P<bin_size>[-\w\d_.]+)/$', 'views.visualize_runtimes'),
     (r'^test/$', 'views.test_view'),
     #(r'^test/(?P<taskname>[-\w\d_.]+)/$', 'views.test_view'),
     )
--- celerymanagementapp/views.py
+++ celerymanagementapp/views.py
@@ -1,6 +1,7 @@
 import datetime
 import calendar
 import time
+import itertools
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -62,8 +63,48 @@ def get_throughput_data(request, taskname=None):
         return HttpResponse(data_table.ToJSonResponse(columns_order=("timestamp","tasks"),req_id=reqId))
     return HttpResponse(data_table.ToJSonResponse(columns_order=("timestamp","tasks")))
 
+def get_runtime_data(request, taskname, search_range=(None,None), runtime_range=(0.,None), bin_size=None, bin_count=None):
+    runtimes = calculate_runtimes(taskname, search_range=search_range, runtime_range=runtime_range, bin_size=float(bin_size), bin_count=int(bin_count))
+    all_data = []
+    description = {}
+    description['bin_name'] = ("string","bin_name")
+    description['count'] = ("number", "count")
+
+    for r in runtimes:
+        data = {}
+        data['bin_name'] = str(r[0][0])+" - " + str(r[0][1])
+        data['count'] = r[1]
+        all_data.append(data)
+    
+    data_table = gviz_api.DataTable(description)
+    data_table.LoadData(all_data)
+
+    if "tqx" in request.GET:        
+        tqx = request.GET['tqx']
+        params = dict([p.split(':') for p in tqx.split(';')])
+        reqId = params['reqId']
+        return HttpResponse(data_table.ToJSonResponse(columns_order=("bin_name","count"),      
+req_id=reqId))
+    return HttpResponse(data_table.ToJSonResponse(columns_order=("bin_name","count")))
+
+def visualize_runtimes(request, taskname, search_range=(None,None), runtime_range=(0.,None), bin_size=None, bin_count=None):
+    return render_to_response('barchart.html',
+            {'task': taskname, 'bin_size': bin_size, 'bin_count': bin_count},
+            context_instance=RequestContext(request))
+
 def visualize_throughput(request, taskname=None):
     return render_to_response('timeseries.html',
             {'task': taskname},
+            context_instance=RequestContext(request))
+
+def view_defined_tasks(request):
+    i = inspect()  
+    workers = i.registered_tasks()
+    defined = set(x for x in itertools.chain.from_iterable(workers.itervalues()))
+    defined = list(defined)
+    defined.sort()
+
+    return render_to_response('tasklist.html',
+            {'tasks':defined},
             context_instance=RequestContext(request))
 
