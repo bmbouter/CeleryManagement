0c7cb9f86a5b324267cd50eb779833147884d0df
David White <dpwhite2@ncsu.edu>
2010-11-04 17:23:57 -0400

    Added a class and some function to aid in dealing with GET parameters in view functions.  Modified the calculate_runtimes function (and associated views) to have the option to auto-calculate the min and max runtime.

--- celerymanagementapp/stats.py
+++ celerymanagementapp/stats.py
@@ -2,6 +2,8 @@ import datetime
 import math
 import itertools
 
+from django.db.models import Max, Min
+
 from djcelery.models import WorkerState, TaskState
 
 def calculate_throughputs(taskname, timerange, interval=1):
@@ -38,14 +40,41 @@ def calculate_throughputs(taskname, timerange, interval=1):
     
     return throughputs
     
+
+def _calculate_runtimes_fillbins(runtimes, runtime_min, bin_size, bin_count):
+    bins = [0 for i in xrange(bin_count)]
+    try:
+        runtime_iter = iter(runtimes)
+        t = runtime_iter.next()
+        for i in xrange(len(bins)):
+            binmax = (i+1)*bin_size+runtime_min
+            while t < binmax:
+                bins[i] += 1
+                t = runtime_iter.next()
+    except StopIteration:
+        # catch exception from raw iter.next() method
+        pass
     
-def _calculate_runtimes_filterobjects(taskname, runtime_range, search_range):
-    if taskname:
-        qs = TaskState.objects.filter(name=taskname, state='SUCCESS', 
-                                      runtime__range=runtime_range)
+    for i in xrange(len(bins)):
+        binmin = i*bin_size+runtime_min
+        binmax = (i+1)*bin_size+runtime_min
+        bins[i] = ((binmin, binmax), bins[i])
+    
+    return bins
+
+    
+def _calculate_runtimes_query(taskname, runtime_range=None, search_range=(None,None)):
+    #import pdb; pdb.set_trace()
+    
+    if runtime_range and runtime_range!=(None,None):
+        if taskname:
+            qs = TaskState.objects.filter(state='SUCCESS', name=taskname, runtime__range=runtime_range)
+        else:
+            qs = TaskState.objects.filter(state='SUCCESS', runtime__range=runtime_range)
+    elif taskname:
+        qs = TaskState.objects.filter(state='SUCCESS', name=taskname)
     else:
-        qs = TaskState.objects.filter(state='SUCCESS', 
-                                      runtime__range=runtime_range)
+        qs = TaskState.objects.filter(state='SUCCESS')
     
     # limit results based on the time that the task ran
     if search_range[0] and search_range[1]:
@@ -54,26 +83,70 @@ def _calculate_runtimes_filterobjects(taskname, runtime_range, search_range):
         qs = qs.filter(tstamp__gte=search_range[0])
     elif search_range[1]:
         qs = qs.filter(tstamp__lte=search_range[1])
+        
+    return qs
+    ##return qs.values_list('runtime', flat=True).order_by('runtime')
     
-    # get sorted list of runtimes
-    return qs.values_list('runtime', flat=True).order_by('runtime')
     
+class RuntimeInfo(object):
+    def __init__(self, vals):
+        self._list = vals
+    def __iter__(self):
+        return self._list.__iter__()
+        
+
+def calculate_auto_runtimes(taskname, search_range=(None,None), bin_count=None):
+    # Get the 
+    runtimeq = _calculate_runtimes_query(taskname, search_range)
+    agg = runtimeq.aggregate(Max('runtime'), Min('runtime'))
+    runtime_min = agg['runtime__min']  if agg['runtime__min'] is not None else  0.
+    runtime_max = agg['runtime__max']  if agg['runtime__max'] is not None else  1.
+    runtimes = runtimeq.values_list('runtime', flat=True).order_by('runtime')
     
-def _calculate_runtimes_fillbins(runtimes, bins, binmaxs):
-    try:
-        runtime_iter = iter(runtimes)
-        t = runtime_iter.next()
-        for i in range(len(bins)):
-            binmax = binmaxs[i]
-            while t < binmax:
-                bins[i] += 1
-                t = runtime_iter.next()
-    except StopIteration:
-        # catch exception from raw iter.next() method
-        pass
+    if bin_count:
+        bin_size = (runtime_max - runtime_min) / bin_count
+    else:
+        e = "Bad arguments to calculate_autoruntimes().  "
+        e += "The argument bin_count must be given."
+        raise RuntimeError(e)
+    
+    bins = _calculate_runtimes_fillbins(runtimes, runtime_min, bin_size, bin_count)
     return bins
     
-def calculate_runtimes(taskname, search_range=(None,None), runtime_range=(0.,None), bin_size=None, bin_count=None):
+    
+def calculate_explicit_runtimes(taskname, search_range=(None,None), 
+                                runtime_range=(0.,None), bin_size=None, 
+                                bin_count=None):
+    runtime_min = runtime_range[0]  if runtime_range[0] else  0.
+    runtime_max = runtime_range[1]  if runtime_range[1] else  0.
+    if bin_size and bin_count:
+        runtime_max = runtime_min + bin_size*bin_count
+    elif runtime_max and bin_size:
+        # use math.ceil so the bins always never exclude runtime_max
+        bin_count = int(math.ceil((runtime_max - runtime_min) / bin_size))
+    elif runtime_max and bin_count:
+        bin_size = (runtime_max - runtime_min) / bin_count
+    else:
+        e = "Bad arguments to calculate_runtimes().  "
+        e += "When the maximum runtime_range is given, "
+        e += "either bin_size or bin_count must be supplied, "
+        e += "and if no maximum runtime_range is given, both must be supplied."
+        raise RuntimeError(e)
+        
+    runtime_range = (runtime_min,runtime_max)
+    
+    runtimeq = _calculate_runtimes_query( taskname, runtime_range, 
+                                          search_range )
+    runtimes = runtimeq.values_list('runtime', flat=True).order_by('runtime')
+    bins = _calculate_runtimes_fillbins(runtimes, runtime_min, bin_size, bin_count)
+    return bins
+    
+    
+
+    
+def calculate_runtimes(taskname, search_range=(None,None), 
+                       runtime_range=(0.,None), bin_size=None, bin_count=None, 
+                       auto_runtime_range=False):
     """
         Calculate the number of tasks that executed within the given runtime_range.
         
@@ -94,36 +167,34 @@ def calculate_runtimes(taskname, search_range=(None,None), runtime_range=(0.,Non
             The size of a bin, in seconds.
         bin_count:
             The number of bins to use.
+        auto_runtime_range:
+            Set the runtime_range automatically absed on other parameters.
             
         Returns a list of tuples where the ith tuple contains information about 
         the ith bin.  The tuples contain: ((binmin,binmax),count).
     """
-    runtime_min = runtime_range[0] if runtime_range[0]  else 0.
-    runtime_max = runtime_range[1] if runtime_range[1]  else 0.
-    if bin_size and bin_count:
-        runtime_max = runtime_min + bin_size*bin_count
-    elif runtime_max and bin_size:
-        # use math.ceil so the bins always never exclude runtime_max
-        bin_count = int(math.ceil((runtime_max - runtime_min) / bin_size))
-    elif runtime_max and bin_count:
-        bin_size = (runtime_max - runtime_min) / bin_count
+    if auto_runtime_range:
+        return calculate_auto_runtimes(taskname, search_range, bin_count)
     else:
-        e = "Bad arguments to calculate_runtimes().  "
-        e += "When the maximum runtime_range is given, "
-        e += "either bin_size or bin_count must be supplied, "
-        e += "and if no maximum runtime_range is given, both must be supplied."
-        raise RuntimeError(e)
-        
-    runtime_range = (runtime_min,runtime_max)
+        return calculate_explicit_runtimes(taskname, search_range, runtime_range, bin_size, bin_count)
     
-    runtimes = _calculate_runtimes_filterobjects(taskname, runtime_range, 
-                                                 search_range)
     
-    binmins = [(i*bin_size+runtime_min) for i in range(bin_count)]
-    binmaxs = [((i+1)*bin_size+runtime_min) for i in range(bin_count)]
-    bins = [0 for i in range(bin_count)]
+    #binmins = [(i*bin_size+runtime_min) for i in range(bin_count)]
+    #binmaxs = [((i+1)*bin_size+runtime_min) for i in range(bin_count)]
+    #bins = [0 for i in range(bin_count)]
     
-    bins = _calculate_runtimes_fillbins(runtimes, bins, binmaxs)
-    return [((a,b),count) for (a,b,count) in itertools.izip(binmins,binmaxs,bins)]
+    #bins = _calculate_runtimes_fillbins(runtimes, bins, binmaxs)
+    #return [((a,b),count) for (a,b,count) in itertools.izip(binmins,binmaxs,bins)]
+    
 
-    
+
+
+
+
+
+
+
+
+
+
+
--- celerymanagementapp/views.py
+++ celerymanagementapp/views.py
@@ -2,6 +2,7 @@ import datetime
 import calendar
 import time
 import itertools
+import urllib
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -20,24 +21,112 @@ import gviz_api
 
 
 #==============================================================================#
+# DATETIME_FMT is used by the datestr_to_datetime() and datetime_to_datestr() 
+# functions.
 # regex corresponding to DATETIME_FMT:  '\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}'
-# YYYY-MM-DD-HH-MM-SS
-    
-DATETIME_FMT = '%Y-%m-%d-%H-%M-%S'
+DATETIME_FMT = '%Y-%m-%d-%H-%M-%S'   # YYYY-MM-DD-HH-MM-SS
 
 def datestr_to_datetime(s):
+    """Convienence function to convert from a string as passed in a URL to a 
+       datetime.datetime object.
+    """
     if s is None:
         return s
     else:
-        return datetime.datetime.strptime(s, fmt)
+        return datetime.datetime.strptime(s, DATETIME_FMT)
     
 def datetime_to_datestr(dt):
-    return dt.strftime(fmt)
+    """Convienence function to convert from a datetime.datetime object to a 
+       string as it would be passed in a URL.
+    """
+    return dt.strftime(DATETIME_FMT)
     
 def searchrange_from_post(post):
+    """ Retrieve the search_range tuple from a request.POST or request.GET 
+        QueryDict.  The tuple will contain None in place of either time (or 
+        both times) if they are not found.
+    """
     min = datestr_to_datetime(post.get('start_time',None))
     max = datestr_to_datetime(post.get('end_time',None))
     return (min,max)
+    
+    
+def string_to_bool(s):
+    s = s.lower()
+    if s=='true' or s=='yes':
+        return True
+    elif s.isdigit():
+        return int(s) != 0
+    return false
+    
+
+def get_postval(postdict, name, convfunc, default=None):
+    """ Retrieve a value from the request.POST or request.GET QueryDict.  The 
+        advantage of using this over using QueryDict.get() is that this 
+        function will also convert the value from a string.
+       
+        postdict:
+            The QueryDict object from which to get the value.
+        name:
+            The key to look up in the postdict.
+        convfunc:
+            A function to convert from the value in the QueryDict (a string) to 
+            the expected value.  Often, a built-in function can do this, as in 
+            the case of int() and float().
+        default:
+            The value to return if the name was not found in the QueryDict.  If 
+            not given, it defaults to None.
+        
+        Example usage:
+            count = get_postval(request.POST, 'count', int, default=0)
+    """
+    try:
+        return convfunc(postdict[name])
+    except KeyError:
+        return default
+    
+
+class QueryStringBuilder(object):
+    """ Class to help create query strings for URLs from within view functions.
+        The values may come from both the view function parameters and from the 
+        request object.
+        
+        To use this class, first create a subclass which defines param_names.  
+        Then, within the view function, construct an instance with the view 
+        function parameters, and call the add_querydict method with request.GET 
+        or request.POST.
+    """
+    param_names = []
+    def __init__(self, **kwargs):
+        """The view function parameters should be given as keyword arguments."""
+        self.args = {}
+        for name in self.param_names:
+            val = kwargs.get(name,'')
+            self.args[name] = str(val)
+    
+    def add_querydict(self, pdict):
+        """Add values from a request.GET or request.POST QueryDict.  Values 
+           will override those given in __init__().
+        """
+        for name in self.param_names:
+            val = pdict.get(name,None)
+            if val is not None:
+                self.args[name] = str(val)
+                
+    def _iter_formatted_params(self):
+        return ('{0}={1}'.format(quote(name), quote(val)) for name,val in self.args if val is not None)
+                
+    def to_querystring(self):
+        """Build a query string from the QueryStringBuilder's values."""
+        quote = urllib.quote
+        def single_query(n, v):
+            return '{0}={1}'.format(quote(n), quote(v))
+        # Join all name/value pairs, except those that have an empty value.
+        r = '&'.join( single_query(name,val) 
+                      for name,val in self.args.iteritems() if val )
+        if r:
+            r = '?' + r
+        return r
 
 
 #==============================================================================#
@@ -86,51 +175,63 @@ def get_throughput_data(request, taskname=None):
         return HttpResponse(data_table.ToJSonResponse(columns_order=("timestamp","tasks"),req_id=reqId))
     return HttpResponse(data_table.ToJSonResponse(columns_order=("timestamp","tasks")))
 
-def get_runtime_data(request, taskname, search_range=(None,None), runtime_min=0., bin_size=None, bin_count=None):
-    runtime_range = (float(runtime_min),None)
-    runtimes = calculate_runtimes(taskname, search_range=search_range, runtime_range=runtime_range, bin_size=float(bin_size), bin_count=int(bin_count))
-    all_data = []
-    description = {}
-    description['bin_name'] = ("string","bin_name")
-    description['count'] = ("number", "count")
 
+def get_runtime_data(request, taskname):
+    search_range =          searchrange_from_post(request.GET)
+    runtime_range =         (get_postval(request.GET, 'runtime_min', float, 0.), None)
+    bin_size =              get_postval(request.GET, 'bin_size', float, None)
+    bin_count =             get_postval(request.GET, 'bin_count', int, None)
+    auto_runtime_range =    get_postval(request.GET, 'auto_runtime_range', 
+                                        string_to_bool, False)
+    runtimes = calculate_runtimes(taskname, search_range=search_range, 
+                                  runtime_range=runtime_range, 
+                                  bin_size=float(bin_size), 
+                                  bin_count=int(bin_count),
+                                  auto_runtime_range=auto_runtime_range)
+    all_data = []
+    description = {
+        'bin_name': ("string","bin_name"),
+        'count':    ("number", "count"),
+        }
     for (runtime_min, runtime_max), count  in  runtimes:
-        data = {}
-        data['bin_name'] = str(runtime_min)+" - " + str(runtime_max)
-        data['count'] = count
+        data = {
+            'bin_name': '{0:g}-{1:g}'.format(runtime_min, runtime_max),
+            'count':    count,
+            }
         all_data.append(data)
     
     data_table = gviz_api.DataTable(description)
     data_table.LoadData(all_data)
     
-    if "tqx" in request.GET:        
+    if "tqx" in request.GET:
         tqx = request.GET['tqx']
         params = dict([p.split(':') for p in tqx.split(';')])
         reqId = params['reqId']
         return HttpResponse(data_table.ToJSonResponse(columns_order=("bin_name","count"), req_id=reqId))
     return HttpResponse(data_table.ToJSonResponse(columns_order=("bin_name","count")))
+    
+
+class RuntimeQueryStringBuilder(QueryStringBuilder):
+    param_names = ['runtime_min', 'bin_count', 'bin_size','start_time',
+                   'end_time','auto_runtime_range']
 
 
 def visualize_runtimes(request, taskname, runtime_min=0., bin_count=None, 
                        bin_size=None):
     
+    qsbuilder = RuntimeQueryStringBuilder(runtime_min=runtime_min, 
+                                          bin_count=bin_count, 
+                                          bin_size=bin_size)
     if "submit" in request.POST:
-        search_range = searchrange_from_post(request.POST)
-        runtime_min = float(request.POST.get('runtime_min', runtime_min))
-        bin_count = int(request.POST.get('bin_count', bin_count))
-        bin_size = float(request.POST.get('bin_size', bin_size))
+        qsbuilder.add_querydict(request.POST)
     else:
-        search_range = (None,None)
+        qsbuilder.add_querydict(request.GET)
+    kwargs = {'taskname': taskname}
     
-    kwargs = {'taskname': taskname,
-              'runtime_min': runtime_min,
-              'bin_count': bin_count,
-              'bin_size': bin_size,
-              }
-                
     url = reverse("celerymanagementapp.views.get_runtime_data", kwargs=kwargs)
+    url += qsbuilder.to_querystring()
     return render_to_response('barchart.html',
-            {'url':url,'taskname':taskname},
+            {'url':url, 'taskname':taskname},
             context_instance=RequestContext(request))
 
 def visualize_throughput(request, taskname=None):
