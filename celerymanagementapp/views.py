5ba8cbaef00d4c9c96f24cf527218b0ce1839fd5
David White <dpwhite2@ncsu.edu>
2010-11-09 16:57:56 -0500

    Modified the HTML templates to use a common base template.

--- celerymanagementapp/views.py
+++ celerymanagementapp/views.py
@@ -103,7 +103,7 @@ class QueryStringBuilder(object):
         self.args = {}
         for name in self.param_names:
             val = kwargs.get(name,'')
-            self.args[name] = str(val)
+            self.args[name] = str(val)  if val is not None else  None
     
     def add_querydict(self, pdict):
         """Add values from a request.GET or request.POST QueryDict.  Values 
@@ -177,7 +177,7 @@ def get_throughput_data(request, taskname=None):
     return HttpResponse(data_table.ToJSonResponse(columns_order=("timestamp","tasks")))
 
 
-def get_runtime_data(request, taskname):
+def get_runtime_data(request, taskname=None):
     search_range =          searchrange_from_post(request.GET)
     runtime_range =         (get_postval(request.GET, 'runtime_min', float, 0.), None)
     bin_size =              get_postval(request.GET, 'bin_size', float, None)
@@ -186,8 +186,8 @@ def get_runtime_data(request, taskname):
                                         string_to_bool, False)
     runtimes = calculate_runtimes(taskname, search_range=search_range, 
                                   runtime_range=runtime_range, 
-                                  bin_size=float(bin_size), 
-                                  bin_count=int(bin_count),
+                                  bin_size=float(bin_size)  if bin_size is not None else  None, 
+                                  bin_count=int(bin_count)  if bin_count is not None else  None,
                                   auto_runtime_range=auto_runtime_range)
     all_data = []
     description = {
@@ -217,7 +217,7 @@ class RuntimeQueryStringBuilder(QueryStringBuilder):
                    'end_time','auto_runtime_range']
 
 
-def visualize_runtimes(request, taskname, runtime_min=0., bin_count=None, 
+def visualize_runtimes(request, taskname=None, runtime_min=0., bin_count=None, 
                        bin_size=None):
     
     qsbuilder = RuntimeQueryStringBuilder(runtime_min=runtime_min, 
@@ -227,7 +227,9 @@ def visualize_runtimes(request, taskname, runtime_min=0., bin_count=None,
         qsbuilder.add_querydict(request.POST)
     else:
         qsbuilder.add_querydict(request.GET)
-    kwargs = {'taskname': taskname}
+    kwargs = {}
+    if taskname:
+        kwargs['taskname'] = taskname
     
     url = reverse("celerymanagementapp.views.get_runtime_data", kwargs=kwargs)
     url += qsbuilder.to_querystring()
@@ -237,7 +239,7 @@ def visualize_runtimes(request, taskname, runtime_min=0., bin_count=None,
 
 def visualize_throughput(request, taskname=None):
     return render_to_response('timeseries.html',
-            {'task': taskname},
+            {'task': taskname, 'taskname': taskname },
             context_instance=RequestContext(request))
 
 def view_defined_tasks(request):
