7129dbc3fded8cafd4251300e452505baab41419
David White <dpwhite2@ncsu.edu>
2010-11-12 09:13:39 -0500

    Fixed runall.py so it recognizes closed processes.  It also now accepts a dict of arguments--the same args as accepted by subprocess.Popen.

--- runall.py
+++ runall.py
@@ -1,7 +1,10 @@
 import subprocess
 import time
+import collections
+import sys
+from optparse import OptionParser
 
-from runall_config import process_arguments
+#from runall_config import process_arguments
     
 class ProcessSet(object):
     def __init__(self, procsargs, init_delay=1.0):
@@ -10,14 +13,22 @@ class ProcessSet(object):
         try:
             for i,args in enumerate(procsargs):
                 print 'ProcessSet: Starting process #{0}'.format(i+1)
-                proc = subprocess.Popen(args)
+                if isinstance(args, collections.Sequence):
+                    proc = subprocess.Popen(args)
+                elif isinstance(args, collections.Mapping):
+                    proc = subprocess.Popen(**args)
+                else:
+                    msg = 'ProcessSet: Bad process arguments!\n'
+                    msg += '  process_arguments in runall_config.py must contain lists and/or dicts.'
+                    msg += '  process_arguments[{0}] is: {1}.'.format(i,type(args))
+                    raise RuntimeError(msg)
                 time.sleep(init_delay)
                 self.procs.append(proc)
         except:
             self.terminate()
             raise
         
-        if any(proc.poll() for proc in self.procs):
+        if not self.all_running():
             self.terminate()
             raise RuntimeError("A process failed during initialization.")
         print 'ProcessSet: Processes started successfully...'
@@ -29,15 +40,15 @@ class ProcessSet(object):
         """Return True if all processes poll() methods return true.  In other 
            words, if they are all stopped.
         """
-        return all(p.poll() for p in self.procs)
+        return all((p.poll() is not None) for p in self.procs)
         
     def all_running(self):
-        return not any(p.poll() for p in self.procs)
+        return all((p.poll() is None) for p in self.procs)
         
     def terminate(self):
         print 'ProcessSet: Terminating processes...'
         for proc in self.procs:
-            if not proc.poll():
+            if proc.poll() is None:
                 try:
                     proc.terminate()
                     proc.wait()
@@ -49,7 +60,7 @@ class ProcessSet(object):
         if not self.all_stopped():
             print 'ProcessSet: Killing processes...'
             for proc in self.procs:
-                if not proc.poll():
+                if proc.poll() is None:
                     try:
                         proc.kill()
                     except OSError:
@@ -57,10 +68,10 @@ class ProcessSet(object):
                         pass
                 
     def loop(self, interval=1.0):
-        print 'ProcessSet: Entering loop...'
+        print 'ProcessSet: Entering loop...  (use CONTROL-C to quit)'
         try:
             while self.all_running():
-                time.sleep(1.0)
+                time.sleep(interval)
                 
         except:
             print 'ProcessSet: Leaving loop on exception...'
@@ -78,18 +89,30 @@ class ProcessSet(object):
             time.sleep(0.5)
             i+=1
         print 'ProcessSet: Allowing extra time for processes to stop before killing them...'
-        time.sleep(2.0)
         self.kill()
         time.sleep(waittime)
 
+def parse_options():
+    parser = OptionParser()
+    parser.add_option("-c", "--config", dest="config",
+                      help="Configuration module", metavar="CONFIG",
+                      default="runall_config")
+    return parser.parse_args()
            
 def main():
-    ps = ProcessSet(process_arguments)
+    opts,args = parse_options()
+    configname = opts.config
+    p = configname.rfind('.')
+    config = __import__(configname, globals(), locals(), [], -1)
+    if p != -1:
+        config = config.getattr(config, configname[p+1:])
+    
+    ps = ProcessSet(config.process_arguments)
     try:
         ps.loop()
     except:
         pass
-    ps.cleanup(waittime=4.0)
+    ps.cleanup(waittime=1.0)
     print 'ProcessSet: Completed!'
 
 
