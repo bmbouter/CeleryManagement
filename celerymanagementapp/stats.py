f62069fb9dddd9de1c30ffd4c91f7b07f3fd81dc
Dennis Kliban <dkliban@ncsu.edu>
2010-11-13 16:27:41 -0500

    Added TaskStats class.

--- celerymanagementapp/stats.py
+++ celerymanagementapp/stats.py
@@ -6,6 +6,178 @@ from django.db.models import Max, Min
 
 from djcelery.models import WorkerState, TaskState
 
+class TaskStats(object):
+    
+    def __init__(self, qs):
+        self.qs = qs
+
+    @property
+    def calculate_throughputs(self, timerange, intervale=1):
+        """ Calculates the throughputs for a given task for each interval over the 
+            given timerange.
+            
+            timerange = Range over which to calculate throughputs.  Must be a tuple 
+                        of two datetime.datetime or datetime.date objects.
+            interval = How often (in seconds) to calculate throughputs.
+            
+            Returns a list of throughputs.  Each throughput is calculated over the 
+            given interval.  The throughputs collectively span the given time 
+            range.
+        """
+        start = timerange[0]
+        stop = timerange[1]
+        if taskname:
+            states_in_range = self.qs.filter(name=taskname, state='SUCCESS', 
+                                                       tstamp__range=(start, stop))
+        else:
+            states_in_range = self.qs.objects.filter(state='SUCCESS', 
+                                                       tstamp__range=(start, stop))
+        
+        # TODO: clean up the following code
+        throughputs = []
+        finterval = float(interval)
+        interval_secs = datetime.timedelta(seconds=interval)
+        mintime = start
+        while mintime < stop:
+            maxtime = mintime + interval_secs
+            qs = states_in_range.filter(tstamp__range=(mintime, maxtime))
+            throughputs.append(qs.count()/finterval)
+            mintime = maxtime
+        
+        return throughputs
+
+    @property
+    def _calculate_runtimes_fillbins(runtimes, runtime_min, bin_size, bin_count):
+        bins = [0 for i in xrange(bin_count)]
+        try:
+            runtime_iter = iter(runtimes)
+            t = runtime_iter.next()
+            for i in xrange(len(bins)):
+                binmax = (i+1)*bin_size+runtime_min
+                while t < binmax:
+                    bins[i] += 1
+                    t = runtime_iter.next()
+        except StopIteration:
+            # catch exception from raw iter.next() method
+            pass
+        
+        for i in xrange(len(bins)):
+            binmin = i*bin_size+runtime_min
+            binmax = (i+1)*bin_size+runtime_min
+            bins[i] = ((binmin, binmax), bins[i])
+        
+        return bins
+
+        
+    def _calculate_runtimes_query(runtime_range=None, search_range=(None,None)):
+        #import pdb; pdb.set_trace()
+        
+        if runtime_range and runtime_range!=(None,None):
+            if taskname:
+                self.qs = self.qs.filter(state='SUCCESS', name=taskname, runtime__range=runtime_range)
+            else:
+                self.qs = self.qs.filter(state='SUCCESS', runtime__range=runtime_range)
+        elif taskname:
+            qs = TaskState.objects.filter(state='SUCCESS', name=taskname)
+        else:
+            qs = TaskState.objects.filter(state='SUCCESS')
+        
+        # limit results based on the time that the task ran
+        if search_range[0] and search_range[1]:
+            self.qs = self.qs.filter(tstamp__range=search_range)
+        elif search_range[0]:
+            self.qs = self.qs.filter(tstamp__gte=search_range[0])
+        elif search_range[1]:
+            self.qs = self.qs.filter(tstamp__lte=search_range[1])
+            
+        return self.qs
+        ##return qs.values_list('runtime', flat=True).order_by('runtime')
+        
+        
+    def calculate_auto_runtimes(search_range=(None,None), bin_count=None):
+        # Get the 
+        runtimeq = _calculate_runtimes_query(search_range)
+        agg = runtimeq.aggregate(Max('runtime'), Min('runtime'))
+        runtime_min = agg['runtime__min']  if agg['runtime__min'] is not None else  0.
+        runtime_max = agg['runtime__max']  if agg['runtime__max'] is not None else  1.
+        runtimes = runtimeq.values_list('runtime', flat=True).order_by('runtime')
+        
+        if bin_count:
+            bin_size = (runtime_max - runtime_min) / bin_count
+        else:
+            e = "Bad arguments to calculate_autoruntimes().  "
+            e += "The argument bin_count must be given."
+            raise RuntimeError(e)
+        
+        bins = _calculate_runtimes_fillbins(runtimes, runtime_min, bin_size, bin_count)
+        return bins
+        
+        
+    def calculate_explicit_runtimes(search_range=(None,None), 
+                                    runtime_range=(0.,None), bin_size=None, 
+                                    bin_count=None):
+        runtime_min = runtime_range[0]  if runtime_range[0] else  0.
+        runtime_max = runtime_range[1]  if runtime_range[1] else  0.
+        if bin_size and bin_count:
+            runtime_max = runtime_min + bin_size*bin_count
+        elif runtime_max and bin_size:
+            # use math.ceil so the bins always never exclude runtime_max
+            bin_count = int(math.ceil((runtime_max - runtime_min) / bin_size))
+        elif runtime_max and bin_count:
+            bin_size = (runtime_max - runtime_min) / bin_count
+        else:
+            e = "Bad arguments to calculate_runtimes().  "
+            e += "When the maximum runtime_range is given, "
+            e += "either bin_size or bin_count must be supplied, "
+            e += "and if no maximum runtime_range is given, both must be supplied."
+            raise RuntimeError(e)
+            
+        runtime_range = (runtime_min,runtime_max)
+        
+        runtimeq = _calculate_runtimes_query(runtime_range, 
+                                              search_range )
+        runtimes = runtimeq.values_list('runtime', flat=True).order_by('runtime')
+        bins = _calculate_runtimes_fillbins(runtimes, runtime_min, bin_size, bin_count)
+        return bins
+    
+    
+
+    
+def calculate_runtimes(taskname, search_range=(None,None), 
+                       runtime_range=(0.,None), bin_size=None, bin_count=None, 
+                       auto_runtime_range=False):
+    """
+        Calculate the number of tasks that executed within the given runtime_range.
+        
+        taskname: 
+            The name of the task to calculate for.  If it evaluates to false, 
+            all tasks are included.
+        search_range: 
+            Limit the tasks to those that completed within this range.  This 
+            should be a pair of datetime.datetime objects.  If either is None, 
+            then no limit is placed on that end of the range.
+        runtime_range:
+            The range of runtimes to include in the result.  If left at the 
+            default (with only a minimum given) then both bin_size and 
+            bin_count must be given.  If the range contains both the min and 
+            max, then only one of bin_size and bin_count must be given.
+            Runtimes are given in seconds, and may be floats.
+        bin_size:
+            The size of a bin, in seconds.
+        bin_count:
+            The number of bins to use.  Required if auto_runtime_range is given.
+        auto_runtime_range:
+            Set the runtime_range automatically absed on other parameters.
+            
+        Returns a list of tuples where the ith tuple contains information about 
+        the ith bin.  The tuples contain: ((binmin,binmax),count).
+    """
+    if auto_runtime_range:
+        return calculate_auto_runtimes(taskname, search_range, bin_count)
+    else:
+        return calculate_explicit_runtimes(taskname, search_range, runtime_range, bin_size, bin_count)
+
+
 def calculate_throughputs(taskname, timerange, interval=1):
     """ Calculates the throughputs for a given task for each interval over the 
         given timerange.
