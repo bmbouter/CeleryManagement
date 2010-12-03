ea71bf85a5378d45588f10dcf8ae86cb102ccc01
edhelms <baelout@gmail.com>
2010-12-03 14:12:39 -0500

    Added new url to ajax.js.

--- celerymanagementapp/media/js/ajax.js
+++ celerymanagementapp/media/js/ajax.js
@@ -2,6 +2,7 @@ var CMACore = (typeof CMACore == "undefined" || !CMACore) ? {} : CMACore;
 
 CMACore.root_url = "/celerymanagementapp/get/"
 CMACore.tasks_url = CMACore.root_url + "tasks/"
+CMACore.workers_url = CMACore.root_url + "workers/"
 
 CMACore.getTasks = function(callbackFunction){
     $.getJSON(CMACore.tasks_url, callbackFunction);
