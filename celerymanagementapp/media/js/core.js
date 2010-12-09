2ca1356dae5c1c672cddb10bf7af72b2a55e7005
Omar Estrella <oyestrel@ncsu.edu>
2010-12-09 10:22:04 -0500

    Removed unecessary code from the core javascript file. Added some javascript files to the HTML header.

--- celerymanagementapp/media/js/core.js
+++ celerymanagementapp/media/js/core.js
@@ -1,33 +1,6 @@
 var CMACore = (typeof CMACore == "undefined" || !CMACore) ? {} : CMACore;
 
-var tasksUrl = '../../get/tasks';
-
 $(document).ready(function() {
-    
     var systemRenderer = new SystemRenderer();
     systemRenderer.init();
-    
-    var xhr = jQuery.getJSON(tasksUrl);
-	var obj = jQuery.parseJSON(xhr.responseText);
-	
-	/*int length = obj.length;
-	
-	for(int i = 0; i < length; i++) {
-	    
-	}
-	
-	$('.content').html();*/
-	
-	//createTable(obj);
 });
-
-function createTable(data) {
-    var table = $('.content');
-    table.html(""); //Empty the content
-    
-    table.html("<table> </table>");
-    
-    data.each(function(i, e) {
-        alert(i + " " + e);
-    });
-}
