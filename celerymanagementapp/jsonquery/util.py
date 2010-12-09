be1d1204f61f1b32d5e2bf080addb1e6c9a9bb50
David White <dpwhite2@ncsu.edu>
2010-12-09 15:18:28 -0500

    Added a list of available DispatchedTask fields to the docs.

--- celerymanagementapp/jsonquery/util.py
+++ celerymanagementapp/jsonquery/util.py
@@ -5,6 +5,8 @@ import time
 # Conversions
 
 def noop_conv(x):
+    """ Dummy do-nothing conversion function for fields that do not require any 
+        conversion between Json and Python types (e.g. str and int). """
     return x
     
 ## Not using the following custom functions because correcting for DST is too 
@@ -21,37 +23,65 @@ def noop_conv(x):
     # x = d - _epoch_datetime
     # return int((x.days*86400 + x.seconds)*1000. + x.microseconds/1000.)
     
-def date_to_python(val):
+def date_to_python(datestamp):
     """ Convert an int representing milliseconds since Jan 1, 1970 to a Python 
-        datetime.date.  The given int must be in UTC.  The return value is in 
-        local time.
+        datetime.date.
+        
+        :param datestamp: An int represeting UTC time in milliseconds since 
+            Jan 1, 1970, or None.
+        
+        :returns: A Python datetime.date object in local time corresponding to 
+            the datestamp parameter, or None if datestamp was None.
     """
-    return datetime.date.fromtimestamp(val/1000.)
+    if datestamp:
+        return datetime.date.fromtimestamp(datestamp/1000.)
+    return datestamp
 
-def datetime_to_python(val):
+def datetime_to_python(timestamp):
     """ Convert an int representing milliseconds since Jan 1, 1970 to a Python 
-        datetime.datetime.  The given int must be in UTC.  The return value is 
-        in local time.
+        datetime.datetime.  
+        
+        :param timestamp: An int represeting UTC time in milliseconds since 
+            Jan 1, 1970, or None.
+        
+        :returns: A Python datetime.datetime object in local time corresponding 
+            to the timestamp parameter, or None if timestamp was None.
     """
-    return datetime.datetime.fromtimestamp(val/1000.)
+    if timestamp:
+        return datetime.datetime.fromtimestamp(timestamp/1000.)
+    return timestamp
     
-def date_from_python(val):
+def date_from_python(dateobj):
     """ Convert a Python datetime.date to an int representing the number of 
-        milliseconds since midnight Jan 1, 1970.  The given date must be in 
-        local time.  The return value is in UTC. 
+        milliseconds since midnight Jan 1, 1970.  
+        
+        :param dateobj: A Python datetime.date object in local time, or None.
+        
+        :returns: An int represeting UTC time in milliseconds since Jan 1, 1970 
+            corresponding to the dateobj parameter, or None if dateobj was None.
     """
-    tt = val.timetuple()
-    unixtime = time.mktime(tt)
-    return int(unixtime*1000)
+    if dateobj:
+        tt = dateobj.timetuple()
+        unixtime = time.mktime(tt)
+        return int(unixtime*1000)
+    return dateobj
     
-def datetime_from_python(val):
+def datetime_from_python(datetimeobj):
     """ Convert a Python datetime.datetime to an int representing the number of 
-        milliseconds since midnight Jan 1, 1970.  The given datetime must be in 
-        local time.  The return value is in UTC. 
+        milliseconds since midnight Jan 1, 1970.  
+        
+        :param datetimeobj: A Python datetime.datetime object in local time, or 
+            None.
+        
+        :returns: An int represeting UTC time in milliseconds since Jan 1, 1970 
+            corresponding to the datetimeobj parameter, or None if datetimeobj 
+            was None.
     """
-    tt = val.timetuple()
-    unixtime = time.mktime(tt)
-    return int(unixtime*1000)
+    if datetimeobj:
+        tt = datetimeobj.timetuple()
+        unixtime = time.mktime(tt)
+        return int(unixtime*1000)
+    return datetimeobj
 
 #==============================================================================#
 
