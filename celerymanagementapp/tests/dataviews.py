3eb0519c3bb020a4e746d7b595db7c906fc307d9
Brian Bouterse <bmbouter@gmail.com>
2011-01-04 13:53:11 -0500

    Added a test case to test dataviews.create_provider

--- celerymanagementapp/tests/dataviews.py
+++ celerymanagementapp/tests/dataviews.py
@@ -39,6 +39,7 @@ class XYDataView_TestCase(base.CeleryManagement_DBTestCaseBase):
 class Configuration_TestCase(base.CeleryManagement_TestCaseBase):
     def setUp(self):
         self.outofbandworker_url = '/celerymanagementapp/outofbandworker/'
+        self.provider_url = '/celerymanagementapp/provider/'
 
     def test_create_outofbandworker(self):
         f = open(testcase_settings.OUTOFBANDWORKER_SSH_KEY_FILE) #path to ssh_key file for testing
@@ -55,6 +56,25 @@ class Configuration_TestCase(base.CeleryManagement_TestCaseBase):
         self.assertEquals(response.status_code, 200)
         outofbandworkers = OutOfBandWorkerNode.objects.all()
         self.assertEquals(len(outofbandworkers), 1)
+
+    def test_create_provider(self):
+        f = open(testcase_settings.PROVIDER_SSH_KEY_FILE) #path to ssh_key file for testing
+        response = self.client.post(self.provider_url, {
+                        'provider_username' : testcase_settings.PROVIDER_USERNAME,
+                        'provider_password' : testcase_settings.PROVIDER_PASSWORD,
+                        'provider_name' : testcase_settings.PROVIDER_NAME,
+                        'image_id' : testcase_settings.PROVIDER_IMAGE_ID,
+                        'username' : testcase_settings.OUTOFBANDWORKER_USERNAME,
+                        'ssh_key' : f,
+                        'celeryd_start_cmd' : testcase_settings.OUTOFBANDWORKER_CELERYD_START ,
+                        'celeryd_stop_cmd' : testcase_settings.OUTOFBANDWORKER_CELERYD_STOP,
+                        'celeryd_status_cmd' : testcase_settings.OUTOFBANDWORKER_CELERYD_STATUS,
+                        'active' : 'on',
+                            })
+        f.close()
+        self.assertEquals(response.status_code, 200)
+        providers = Provider.objects.all()
+        self.assertEquals(len(providers), 1)
 
 
 # class WorkerSubprocessesDataview_TestCase(base.CeleryManagement_DBTestCaseBase):
