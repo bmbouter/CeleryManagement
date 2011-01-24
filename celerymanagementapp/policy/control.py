326ba25e2f3a900f0317893c0ee0d1376e6bcb33
David White <dpwhite2@ncsu.edu>
2011-01-23 21:15:30 -0500

    Removed commented code.

--- celerymanagementapp/policy/control.py
+++ celerymanagementapp/policy/control.py
@@ -12,10 +12,11 @@ def _get_task_class(name):
     assert isinstance(task, BaseTask), 'type(task): {0}'.format(type(task))
     return task.__class__
 
+
 @Panel.register
 def update_tasks_settings(panel, tasks_settings):
+    # task_settings = dict: { taskname: { attrname: value, ... }, ... }
     panel.logger.debug('policy.control: update_tasks_settings()')
-    ##print 'policy.control: update_tasks_settings()'
     for taskname, settings in tasks_settings.iteritems():
         # TODO: wrap this in try..except so one failure doesn't stop setting all settings.
         taskcls = _get_task_class(taskname)
@@ -25,9 +26,10 @@ def update_tasks_settings(panel, tasks_settings):
     
 
 @Panel.register
-def get_task_settings(panel, tasknames, setting_names):
+def get_task_settings(panel, tasknames=None, setting_names=None):
     panel.logger.debug('policy.control: get_task_settings()')
-    ##print 'policy.control: get_task_settings()'
+    if tasknames is None:
+        tasknames = tasks_registry.data
     allsettings = {}
     for taskname in tasknames:
         taskcls = _get_task_class(taskname)    
@@ -40,10 +42,8 @@ def get_task_settings(panel, tasknames, setting_names):
 @Panel.register
 def get_all_task_settings(panel, setting_names):
     panel.logger.debug('policy.control: get_all_task_settings()')
-    ##print 'policy.control: get_all_task_settings()'
     allsettings = {}
     for taskname in tasks_registry.data:
-        ##print 'taskname: {0}'.format(taskname)
         taskcls = _get_task_class(taskname)    
         data = dict((attr, taskcls.__dict__[attr]) for attr in setting_names 
                                                    if attr in taskcls.__dict__)
@@ -53,9 +53,10 @@ def get_all_task_settings(panel, setting_names):
 
 @Panel.register
 def restore_task_settings(panel, restore_data):
+    # restore_data = dict: { taskname: (restore_dict, erase_list), ... }
     panel.logger.debug('policy.control: restore_task_settings()')
-    ##print 'policy.control: restore_task_settings()'
     for taskname, (restore, erase) in restore_data.iteritems():
+        # TODO: catch taskname not found exceptions
         taskcls = _get_task_class(taskname)
         for attr,v in restore.iteritems():
             setattr(taskcls, attr, v)
@@ -69,7 +70,6 @@ def restore_task_settings(panel, restore_data):
 @Panel.register
 def get_task_attribute(panel, taskname, attrname):
     panel.logger.debug('policy.control: get_task_attribute()')
-    ##print 'policy.control: get_task_attribute()'
     try:
         task = _get_task_class(taskname)
     except KeyError:
@@ -89,7 +89,6 @@ def get_task_attribute(panel, taskname, attrname):
 @Panel.register
 def set_task_attribute(panel, tasknames, attrname, value):
     panel.logger.debug('policy.control: set_task_attribute()')
-    ##print 'policy.control: set_task_attribute()'
     if isinstance(tasknames, basestring):
         tasknames = [tasknames]
     for taskname in tasknames:
@@ -120,4 +119,5 @@ def prefetch_decrement(panel, n):
     panel.consumer.qos.decrement(n)
     return {'ok': 'decremented prefetch by {0}'.format(n) }
 
+#==============================================================================#
 
