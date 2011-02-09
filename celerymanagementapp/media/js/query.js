47ec285035e3363dc2d5532e3314770b901a6aa3
Omar Estrella <oyestrel@ncsu.edu>
2011-02-09 13:53:34 -0500

    Added a datepicker for certiain time data. Added units to the range interval in order to correctly display intervals for certain time data.

--- celerymanagementapp/media/js/query.js
+++ celerymanagementapp/media/js/query.js
@@ -4,15 +4,52 @@ if(CMA.Core.testUrls) {
     CMA.Core.ajax.loadUrls(); 
 }
 
-var c1 = null;
+var c1 = null,
+    time_factors_milliseconds = {
+        seconds: 1000,
+        minutes: 60000, 
+        hours: 3600000, 
+        days: 86400000,
+        months: 2.62974383e9,
+        years: 3.1556926e10
+    },
+    time_factors_seconds = {
+        seconds: 1,
+        minutes: 60, 
+        hours: 3600, 
+        days: 86400,
+        months: 2.62974383e6,
+        years: 3.1556926e7
+    };
 
-$(document).ready(function() {
+$(document).ready(onReady);
+
+function onReady() {
     System.Handlers.loadHandlers();
     
     var q = {"segmentize":{"field":"worker","method":["all"]},"aggregate":[{"field":"waittime","methods":["average","max","min"]}]};
     
     submitQuery(JSON.stringify(q));
-});
+};
+
+function parseDate(field) {
+    var jsonDate = new AnyTime.Converter(
+            {format: '{"year":"%Y","month":"%m","day":"%d","hour":"%H","minute":"%i","second":"%s"}'}
+        ),
+        converter = new AnyTime.Converter(),
+        parsedDate = null,
+        date = null;
+    
+    parsedDate = converter.parse($(field).val());
+    obj = JSON.parse(jsonDate.format(parsedDate));
+    //console.log(obj);
+    
+    date = new Date(parseFloat(obj.year), parseFloat(obj.month), parseFloat(obj.day), parseFloat(obj.hour),
+                    parseFloat(obj.minute), parseFloat(obj.second));
+    //console.log(date.getTime());
+    
+    return date.getTime();
+};
 
 function startChart(data) {
     var options = CMA.Core.DataParser.getTicks();
@@ -26,11 +63,24 @@ function submitQuery(query) {
 }
 
 function formatData(response) {
+    var i,
+        field = $('#segmentize_field');
+    
     if(CMA.Core.testUrls) {
         response = JSON.parse(response);
     }
     
-    System.EventBus.fireEvent('formatData', response);
+    if(field.val() === 'sent' || field.val() === 'received' || field.val() === 'started' ||
+                field.val() === 'succeeded' || field.val() === 'failed') {
+        var data = response.data[0];
+        var length = data.length;
+        
+        for(i = 0; i < length; i++) {
+            data[i] = toRelativeTime(data[i], $('#interval_select').val());
+        }
+    } else {
+        System.EventBus.fireEvent('formatData', response);
+    }
 }
 
 function create_query() {
@@ -64,23 +114,37 @@ function create_query() {
         });
     }
     
-    if($('#segmentize').attr('checked')) {
+    //if($('#segmentize').attr('checked')) {
         object.segmentize = { };
         object.segmentize.field = $('#segmentize_field').val();
         object.segmentize.method = [ $('#segmentize_method').val() ];
+        
+        var field = $('#segmentize_field');
         
         if($('#segmentize_method').val() == 'range') {
             object.segmentize.method.push({});
             
             $.each($('#segmentize_range').children('div').children('input'), function(i, v) {
-                object.segmentize.method[1][$(v).attr('name')] = parseFloat($(v).val());
+                if(field.val() === 'runtime' || field.val() === 'totaltime' || field.val() === 'waittime') {
+                    if($(v).attr('name') !== 'interval') {
+                        object.segmentize.method[1][$(v).attr('name')] = parseFloat($(v).val());
+                    } else {
+                        object.segmentize.method[1][$(v).attr('name')] = toSeconds($(v).val(), $('#interval_select').val());
+                    }
+                } else {
+                    if($(v).attr('name') !== 'interval') {
+                        object.segmentize.method[1][$(v).attr('name')] = parseDate(v);
+                    } else {
+                        object.segmentize.method[1][$(v).attr('name')] = toMilliseconds($(v).val(), $('#interval_select').val());
+                    }
+                }
             });
         } else if($('#segmentize_method').val() == 'values') {
             object.segmentize.method.push($('#segmentize_values').children('div').children('input').val().split(/[\s,]+/));
         }
-    }
+    //}
     
-    if($('#aggregate').attr('checked')) {
+    //if($('#aggregate').attr('checked')) {
         object.aggregate = [ ];
         
         var fields = $('#aggregate_table tr');
@@ -93,10 +157,82 @@ function create_query() {
                 object.aggregate[i - 1].methods = $.text([children[1]]).split(/[\s,]+/);
             }
         });
-    }
+    //}
     
     console.log(JSON.stringify(object));
     submitQuery(JSON.stringify(object));
+}
+
+function toMilliseconds(time, type) {
+    if(type === 'seconds') {
+        return time * time_factors_milliseconds.seconds;
+    } else if(type === 'minutes') {
+        return time * time_factors_milliseconds.minutes;
+    } else if(type === 'hours') {
+        return time * time_factors_milliseconds.hours;
+    } else if(type === 'days') {
+        return time * time_factors_milliseconds.days;
+    } else if(type === 'months') {
+        return time * time_factors_milliseconds.months;
+    } else if(type === 'years') {
+        return time * time_factors_milliseconds.years;
+    } else {
+        return null;
+    }
+}
+
+function toSeconds(time, type) {
+    if(type === 'seconds') {
+        return time * time_factors_seconds.seconds;
+    } else if(type === 'minutes') {
+        return time * time_factors_seconds.minutes;
+    } else if(type === 'hours') {
+        return time * time_factors_seconds.hours;
+    } else if(type === 'days') {
+        return time * time_factors_seconds.days;
+    } else if(type === 'months') {
+        return time * time_factors_seconds.months;
+    } else if(type === 'years') {
+        return time * time_factors_seconds.years;
+    } else {
+        return null;
+    }
+}
+
+function toRelativeTimeMilliseconds(time, type) {
+    if(type === 'seconds') {
+        return time / time_factors_milliseconds.seconds;
+    } else if(type === 'minutes') {
+        return time / time_factors_milliseconds.minutes;
+    } else if(type === 'hours') {
+        return time / time_factors_milliseconds.hours;
+    } else if(type === 'days') {
+        return time / time_factors_milliseconds.days;
+    } else if(type === 'months') {
+        return time / time_factors_milliseconds.months;
+    } else if(type === 'years') {
+        return time / time_factors_milliseconds.years;
+    } else {
+        return null;
+    }
+}
+
+function toRelativeTimeSeconds(time, type) {
+    if(type === 'seconds') {
+        return time / time_factors_seconds.seconds;
+    } else if(type === 'minutes') {
+        return time / time_factors_seconds.minutes;
+    } else if(type === 'hours') {
+        return time / time_factors_seconds.hours;
+    } else if(type === 'days') {
+        return time / time_factors_seconds.days;
+    } else if(type === 'months') {
+        return time / time_factors_seconds.months;
+    } else if(type === 'years') {
+        return time / time_factors_seconds.years;
+    } else {
+        return null;
+    }
 }
 
 //Query options panel event listeners
@@ -125,13 +261,14 @@ $(function() {
     }
     
     function change_segmentize_method() {
-        var that = $('#segmentize_method');
+        var method = $('#segmentize_method'),
+            field = $('#segmentize_field');
         
-        if(that.val() == 'range') {
+        if(method.val() == 'range') {
             $('#segmentize_range').show();
             $('#segmentize_values').hide();
             $('#segmentize_taskname').hide();
-        } else if(that.val() == 'values') {
+        } else if(method.val() == 'values') {
             $('#segmentize_values').show()
             $('#segmentize_range').hide();
             $('#segmentize_taskname').hide();
@@ -142,11 +279,21 @@ $(function() {
     }
     
     function change_segmentize_field() {
-        var enum = $('#segmentize_field optgroup[id=enum]'),
-            time = $('#segmentize_field optgroup[id=time]'),
-            range = $('#segmentize_method option[value=range]');
+        $('#range_min').AnyTime_noPicker();
+        $('#range_max').AnyTime_noPicker();
         
-        if(enum.children('[value=' + $(this).val() + ']').length !== 0) {
+        var _enum = $('#segmentize_field optgroup[id=enum]'),
+            time = $('#segmentize_field optgroup[id=time]'),
+            range = $('#segmentize_method option[value=range]'),
+            field = $('#segmentize_field');
+        
+        if(field.val() === 'sent' || field.val() === 'received' || field.val() === 'started' ||
+                field.val() === 'succeeded' || field.val() === 'failed') {
+            $('#range_min').AnyTime_picker({format: "%Y-%m-%d %T"});
+            $('#range_max').AnyTime_picker({format: "%Y-%m-%d %T"});
+        }
+        
+        if(_enum.children('[value=' + $(this).val() + ']').length !== 0) {
             range.attr('disabled', 'disabled');
             range.next().attr('selected', 'selected');
             change_segmentize_method();
@@ -157,6 +304,12 @@ $(function() {
         }
     }
     
+    $('#segmentize_field').change(change_segmentize_field);
+    
+    $('#segmentize_method').change(change_segmentize_method);
+    
+    $('#add_aggregation').click(add_aggregation);
+    
     $(':checkbox').change(function() {
         if($(this).attr('checked')) {
             $(this).siblings('div').show();
@@ -164,12 +317,6 @@ $(function() {
             $(this).siblings('div').hide();
         }
     });
-    
-    $('#segmentize_field').change(change_segmentize_field);
-    
-    $('#segmentize_method').change(change_segmentize_method);
-    
-    $('#add_aggregation').click(add_aggregation);
     
     $('#filter_option').change(function() {
         var field = $('#filter_arg2');
@@ -190,6 +337,9 @@ $(function() {
             field.hide().next().hide();
         }
     });
+    
+    //$('#range_min').AnyTime_picker({format: "%Y-%m-%d %T"});
+    //$('#range_max').AnyTime_picker({format: "%Y-%m-%d %T"});
 });
 
 //Chart options panel event listeners
@@ -261,4 +411,3 @@ $(function() {
         }
     });
 });
-
