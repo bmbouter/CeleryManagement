40b27c33184c400df15393152722dd160b612a2c
edhelms <ericdhelms@gmail.com>
2011-02-16 16:21:15 -0500

    Added error output to login template. Added logout view. New setting to change default LOGIN_URL.

--- celerymanagementapp/urls.py
+++ celerymanagementapp/urls.py
@@ -102,6 +102,7 @@ urlpatterns += patterns('celerymanagementapp',
 
 urlpatterns += patterns('',
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'celerymanagementapp/login.html'}),
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/celerymanagementapp/login/'}),
 )
 
 urlpatterns += patterns('celerymanagementapp.views',
